0090964b005e8fe852602eeaea80c2f13d7195c1
kaulkikass <kaulkikass@gmail.com>
2022-07-27 20:53:16 +0300

    parcelMachines, Ebay products, Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,69 @@
+import { useRef, useState } from "react";
+import omnivaParcelMachines from '../omniva.json';
+
 function Cart() {
-    return ( <div></div> );
+    const [cart, setCart] = useState(JSON.parse(sessionStorage.getItem('cart')) || []);
+    const parcelMachines = omnivaParcelMachines.filter(element => element.A0_NAME === 'EE');
+
+    const increaseQuantity = (index) => {
+        cart[index].quantity = cart[index].quantity + 1;
+        setCart(cart.slice());
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    const decreaseQuantity = (index) => {
+        cart[index].quantity = cart[index].quantity - 1;
+        if (cart[index].quantity <= 0) {
+            deleteProduct(index);
+        }
+        setCart(cart.slice());
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    const deleteProduct = (index) => {
+        cart.splice(index,1);
+        setCart(cart.slice());
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    const calculateCartSum = () => {
+     let cartSum = 0;
+     cart.forEach(element => cartSum = cartSum + element.product.price * element.quantity);
+     return cartSum;
+    }
+
+    const [selectedPM, setSelectedPM] = useState(sessionStorage.getItem('parcelMachine') || '');
+    const pmRef = useRef();
+
+    const selectPM = () => {
+        setSelectedPM(pmRef.current.value);
+        sessionStorage.setItem('parcelMachine', pmRef.current.value);
+    }
+
+    const unSelectPM = () => {
+        setSelectedPM('');
+        sessionStorage.removeItem('parcelMachine');
+    }
+
+    return ( 
+    <div>
+        {cart.map((element,index) => 
+        <div>
+            <img src={element.product.image} alt="" />
+            <div>{element.product.name}</div>
+            <div>{element.product.price}</div>
+            <button onClick={() => decreaseQuantity(index)}>-</button>
+            <div>{element.quantity}</div>
+            <button onClick={() => increaseQuantity(index)}>+</button>
+            <div>{element.quantity * element.product.price}</div>
+            <button onClick={() => deleteProduct(index)}>x</button>
+        </div>)}
+
+        {selectedPM === '' && <select onChange={selectPM} ref={pmRef}>{parcelMachines.map(element => <option>{element.NAME}</option>)}</select>}
+        {selectedPM !== '' && <div>{selectedPM} <button onClick={() => unSelectPM()}>X</button></div>}
+        <div>{calculateCartSum()} $</div>
+    </div> 
+    );
 }
 
 export default Cart;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,106 @@
+import productsFromFile from '../products.json';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
 function HomePage() {
-    return ( <div></div> );
+    const [products, setProducts] = useState(productsFromFile);
+                    //---.map tagastab -- returns
+    const categories =[...new Set(productsFromFile.map(element => element.category))];
+    const [selectedCategory, setSelectedCategory] = useState('all');
+
+
+    //[].map ( => uus_väärtus)  
+    //[].sort( => pluss/miinus ) võrdleb, kas tuleb miinus märgiga või plussiga tehe
+    //[].filter( => true/false )  vaatab, kas vaste sobib voi ei
+
+    //sort muteerib --mutates
+    const sortAZ = () => {
+        // muteerib --- mutates
+        const result = [...products].sort((a,b)=> a.name.localeCompare(b.name));
+        setProducts(result);
+      }
+    
+      const sortZA = () => {
+        const result = [...products].sort((a,b)=> b.name.localeCompare(a.name));
+        setProducts(result);
+      }
+    
+      const sortPriceAsc = () => {
+        const result = [...products].sort((a,b)=> a.price - b.price);
+        setProducts(result);
+      }
+
+      const sortPriceDesc = () => {
+        const result = [...products].sort((a,b)=> b.price - a.price);
+        setProducts(result);
+      }
+
+    const filterByCategory = (catergoryClicked) => {
+        if (catergoryClicked === 'all') {
+            setProducts(productsFromFile);
+        } else {
+            const result = productsFromFile.filter(element => element.category === catergoryClicked)
+            setProducts(result);
+        }
+        setSelectedCategory(catergoryClicked);
+    }
+
+    // {product: {id, name, category} quantity: 1} objekt objekti sees
+    const addToCart = (productClicked) => {
+        let cart = sessionStorage.getItem('cart');
+        cart = JSON.parse(cart) || [];
+        const index = cart.findIndex(element => element.product.id === productClicked.id);
+        if (index >= 0) {
+            //suurendan kogust
+            //massiivi muutmine --->
+            // ---> ['ant', 'bison', 'camel'][1] = 'bird'
+            // -----> ['ant', 'bird', 'camel']
+            cart[index].quantity = cart[index].quantity + 1;
+        } else {
+            //pushi sisse kirjutan, mida lisan lõppu, kui toodet ei ole veel ostukorvis
+            // [].push({product: {id:, name:, category:}, quantity:1})
+           cart.push({product: productClicked, quantity: 1}); 
+        }
+        cart = JSON.stringify(cart);
+        sessionStorage.setItem('cart', cart);
+    }
+    return ( 
+    <div>
+        <div 
+        className={selectedCategory === 'all' ? 'active-category' : undefined} 
+        onClick={() => filterByCategory('all')}>
+            Kõik kategooriad
+        </div>
+        { categories.map(element =>  
+            <div
+                className={selectedCategory === element ? 'active-category' : undefined}
+             key={element} onClick={() => filterByCategory(element)}>
+                {element}
+            </div>) }        
+        <DropdownButton id="dropdown-basic-button" title="Sorteeri ">
+            <Dropdown.Item onClick={() => sortAZ()}>A - Z</Dropdown.Item>
+            <Dropdown.Item onClick={() => sortZA()}>Z - A</Dropdown.Item>
+            <Dropdown.Item onClick={() => sortPriceAsc()}>
+            Kasvav hind
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => sortPriceDesc()}>
+            Kahanev hind
+            </Dropdown.Item>
+        </DropdownButton>
+        <div>{products.length} toodet leitud</div>
+        {products.map(element => 
+        <div key={element.id}>
+            <img src={element.image} alt="" />
+            <div>{element.name}</div>
+            <div>{element.price}</div>
+            <Button variant='success' onClick={() => addToCart(element)}>Lisa ostukorvi</Button>
+        </div>
+        )};
+    </div>
+    );
 }
 
 export default HomePage;
+
