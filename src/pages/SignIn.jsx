150a8366af20c451988313b3f97f337c2dff872e
kaulkikass <kaulkikass@gmail.com>
2022-08-25 17:54:38 +0300

    Update signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,20 @@
 import { useRef } from "react";
 
 function SignIn() {
-    const emailRef= useRef();
-    const passwordRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const login = () => {}
-    return ( 
-    <div>
-        <label>Email</label> <br />
-        <input type="text" />
-    </div> 
-    );
+  const login = () => {
+      // FIREBASE AUTHENTICATION
+  }
+
+  return ( <div>
+    <label>Email</label> <br />
+    <input ref={emailRef} type="text" /> <br />
+    <label>Parool</label> <br />
+    <input ref={passwordRef} type="password" /> <br />
+    <button onClick={login}>Logi sisse</button>
+  </div> );
 }
 
 export default SignIn;
