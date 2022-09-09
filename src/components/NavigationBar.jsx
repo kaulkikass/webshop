d39eb6b27bfb18fa3f63749113c629fd8165f553
kaulkikass <kaulkikass@gmail.com>
2022-09-10 00:00:58 +0300

    Revert "UseContext, log in, admin panel show/hide, log out, add user"
    This reverts commit df9a58496c76ecd0df809985fa36210dedb0fb11.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,45 +1,59 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { cartSumService } from '../store/cartSumService';
+/* import { cartSumService } from '../store/cartSumService'; */
+import { useState } from 'react';
+import { useContext } from 'react';
+import AuthContext from '../store/AuthContext';
+import CartSumContext from '../store/CartSumContext';
 
 function NavigationBar() {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const authCtx = useContext(AuthContext);
+  const cartSumCtx = useContext(CartSumContext);
 
-    const changeWebsiteLanguage = (language) => {
-      i18n.changeLanguage(language); 
-      localStorage.setItem('language', language);
-    }
+ /*  const calculateCartSum = () => {
+    let cart = sessionStorage.getItem("cart");
+    cart = JSON.parse(cart) || [];
+    let cartSum = 0;
+    cart.forEach(element => cartSum = cartSum + element.product.price * element.quantity );
+    return cartSum;
+  }
+ */
+/*   const [cartSum, setCartSum] = useState(calculateCartSum()); */
 
-    const calculateCartSum = () => {
-        let cart = sessionStorage.getItem("cart");
-        cart = JSON.parse(cart) || [];
-        let cartSum = 0;
-        cart.forEach(element => cartSum = cartSum + element.product.price * element.quantity );
-        return cartSum;
-      }
+  const changeWebsiteLanguage = (language) => {
+    i18n.changeLanguage(language);
+    localStorage.setItem("language", language);
+  }
 
-    const [cartSum, setCartSum] = useState(calculateCartSum());
+/*   cartSumService.getCartSum().subscribe(newCartSum => setCartSum(newCartSum)); */
 
-    cartSumService.getCartSum().subscribe(newCartSum => setCartSum(newCartSum));
+  const logout = () => {
+    // globaalne muutuja false-ks
+    authCtx.logout();
+  }
 
-    return (       
+  return ( 
     <Navbar bg="light" variant="light">
-        <Container>
-            <Navbar.Brand as={Link} to='/'>Webshop</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to='/admin'>{t('navbar.admin-button')}</Nav.Link>
-                <Nav.Link as={Link} to='/meist'>{t('navbar.about-button')}</Nav.Link>
-                <Nav.Link as={Link} to='/poed'>{t('navbar.shops-button')}</Nav.Link>
-                <Nav.Link as={Link} to='/ostukorv'>{t('navbar.cart-button')}</Nav.Link>
-            </Nav>
-        </Container>
-        <div>{cartSum.toFixed(2)}$</div>
-        <img className='lang' onClick={() => changeWebsiteLanguage('en')} src={require('../assets/english.png')} alt='' />
-        <img className='lang' onClick={() => changeWebsiteLanguage('ee')} src={require('../assets/estonian.png')} alt='' />
-        <img className='lang' onClick={() => changeWebsiteLanguage('ru')} src={require('../assets/russian.png')} alt='' />
-    </Navbar> );
+      <Container>
+        <Navbar.Brand as={Link} to="/">Webshop</Navbar.Brand>
+        <Nav className="me-auto">
+          {authCtx.loggedIn === true && <Nav.Link as={Link} to="/admin">{t('navbar.admin-button')}</Nav.Link>}
+          <Nav.Link as={Link} to="/meist">{t('navbar.about-button')}</Nav.Link>
+          <Nav.Link as={Link} to="/poed">{t('navbar.shops-button')}</Nav.Link>
+          <Nav.Link as={Link} to="/ostukorv">{t('navbar.cart-button')}</Nav.Link>
+          { authCtx.loggedIn === false && <Nav.Link as={Link} to="/logi-sisse">Logi sisse</Nav.Link>}
+          { authCtx.loggedIn === true && <Nav.Link onClick={logout}>Logi välja</Nav.Link>}
+
+        </Nav>
+      </Container>
+      <div>{cartSumCtx.cartSum.toFixed(2)} €</div>
+      <img className='lang' onClick={() => changeWebsiteLanguage('en')} src={require('../assets/english.png')} alt="" />
+      <img className='lang' onClick={() => changeWebsiteLanguage('ee')} src={require('../assets/estonian.png')} alt="" />
+      <img className='lang' onClick={() => changeWebsiteLanguage('ru')} src={require('../assets/russian.png')} alt="" />
+    </Navbar>
+   );
 }
 
 export default NavigationBar;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,72 +1,113 @@
+import { useContext } from "react";
 import { useState } from "react";
 import ParcelMachine from "../components/cart/ParcelMachine";
 import Payment from "../components/cart/Payment";
-/* import omnivaParcelMachines from '../omniva.json'; */
 import styles from '../css/Cart.module.css';
-import { cartSumService } from '../store/cartSumService';
-
-
+import CartSumContext from "../store/CartSumContext";
+// import { cartSumService } from '../store/cartSumService';
 
 function Cart() {
-    const [cart, setCart] = useState(JSON.parse(sessionStorage.getItem('cart')) || []);
+  const [cart, setCart] = useState(JSON.parse(sessionStorage.getItem("cart")) || []);
+  const cartSumCtx = useContext(CartSumContext);
 
-    const increaseQuantity = (index) => {
-        cart[index].quantity = cart[index].quantity + 1;
-        setCart(cart.slice());
-        sessionStorage.setItem('cart', JSON.stringify(cart));
-        cartSumService.sendCartSum(calculateCartSum());
-        
+  const decreaseQuantity = (index) => {
+    cart[index].quantity = cart[index].quantity - 1;
+    if (cart[index].quantity <= 0) {
+      removeFromCart(index);
     }
+    setCart(cart.slice());
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    // cartSumService.sendCartSum(calculateCartSum());
+    cartSumCtx.setCartSum(calculateCartSum());
+  }
 
-    const decreaseQuantity = (index) => {
-        cart[index].quantity = cart[index].quantity - 1;
-        if (cart[index].quantity <= 0) {
-            deleteProduct(index);
-        }
-        setCart(cart.slice());
-        sessionStorage.setItem('cart', JSON.stringify(cart));
-        cartSumService.sendCartSum(calculateCartSum());
-    }
+  const increaseQuantity = (index) => {
+    cart[index].quantity = cart[index].quantity + 1; // võtab varasema koguse ja liidab ühe juurde
+    setCart(cart.slice());
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    // cartSumService.sendCartSum(calculateCartSum());
+    cartSumCtx.setCartSum(calculateCartSum());
+  }
 
-    const deleteProduct = (index) => {
-        cart.splice(index,1);
-        setCart(cart.slice());
-        sessionStorage.setItem('cart', JSON.stringify(cart));
-        cartSumService.sendCartSum(calculateCartSum());
-    }
+  const removeFromCart = (index) => {
+    cart.splice(index,1);
+    setCart(cart.slice());
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    // cartSumService.sendCartSum(calculateCartSum());
+    cartSumCtx.setCartSum(calculateCartSum());
+  }
 
-    const calculateCartSum = () => {
-     let cartSum = 0;
-     cart.forEach(element => cartSum = cartSum + element.product.price * element.quantity);
-     return cartSum;
-    }
+  const calculateCartSum = () => {
+    let cartSum = 0;
+    cart.forEach(element => cartSum = cartSum + element.product.price * element.quantity )
+    return cartSum;
+  }
 
- 
+  return ( 
+  <div>
+    {/* TEHKE KA TÜHJENDA OSTUKORV, MIDA NÄIDATAKSE VAID SIIS KUI OSTUKORVIS ON MIDAGI */}
+    <button>Kodus: tühjenda</button>
 
+    {cart.map((element, index) => 
+      <div className={styles.product} key={element.product.id}>
+        <img className={styles.image} src={element.product.image} alt="" />
+        <div className={styles.name}>{element.product.name}</div>
+        <div className={styles.price}>{Number(element.product.price).toFixed(2)} €</div>
+        <div className={styles.controls}>
+          <img 
+            className={styles.button} 
+            onClick={() => decreaseQuantity(index)} 
+            src={require("../assets/minus-button.png")} alt="" />
+          <div>{element.quantity} tk</div>
+          <img 
+            className={styles.button} 
+            onClick={() => increaseQuantity(index)}
+            src={require("../assets/add.png")} alt="" />
+        </div>
+        <div className={styles.total}>{(element.product.price * element.quantity).toFixed(2)} €</div>
+        <img 
+          className={styles.button} 
+          onClick={() => removeFromCart(index)} 
+          src={require("../assets/cancel.png")} alt="" />
+      </div>)}
 
-    return ( 
-    <div>
-        {cart.map((element,index) => 
-        <div className={styles.product} key={element.id}>
-            <img className={styles.image} src={element.product.image} alt="" />
-            <div className={styles.name}>{element.product.name}</div>
-            <div className={styles.price}>{element.product.price.toFixed(2)} $</div>
-            <div className={styles.controls}>
-                <img className={styles.button} onClick={() => decreaseQuantity(index)} src={require('../assets/minus-button.png')} alt='minus-sign' />
-                <div>{element.quantity} tk</div>
-                <img className={styles.button} onClick={() => increaseQuantity(index)} src={require('../assets/add.png')} alt="plus-sign"/>
-            </div>
-            <div className={styles.total}>{(element.quantity * element.product.price).toFixed(2)} $</div>
-            <img className={styles.button} onClick={() => deleteProduct(index)} src={require('../assets/cancel.png')} alt='remove'/>
-        </div>)}
-        { cart.length > 0 && 
-        <div className={styles.sum}>
-           <ParcelMachine />
+    { cart.length > 0 && 
+    <div className={styles.sum}>
+       <ParcelMachine />
+       <Payment totalSum={calculateCartSum()} />
        <div>{calculateCartSum().toFixed(2)} €</div>
-            <Payment totalSum={calculateCartSum()} />
+       
     </div>}
-    </div> 
-    );
+
+     {/* KODUS: näita mingit pilti kui ostukorv on tühi */}
+  </div> );
 }
 
 export default Cart;
+
+
+// Kõik failid alla 200 rea
+// Ideaalis proovime hoida alla 150
+// Hoida kõik failid alla 100 rea
+
+
+
+
+// 70ak/h / 4   17.5
+// 22.08 1. Pagination / pildi lisamine failina / props+component
+// 24.08 2. Props+component jätk + ostukorvi kogusumma navbaris
+// 29.08 3. sisselogimine/registreerumine
+// 31.08 4. Nortali proovitöö
+// 14.09 17.30-19.00  5. Projekti esitlemine  0.5  --> näitate lehte + koodi
+
+// Webshopis
+// Wordpress  headless
+// Iseseisvad API otspunkti ülesanded
+// Mongo DB
+// Wordpress
+// Keelevahetusel ka URL keele vahetus
+// Sorteerimise salvestus kategooria vahetamisel
+// Pakiautomaadil vahetus - 2 refi?, 2 onChange?
+// Lazy-loading
+// Mitu ostukorvis on avalehel
+// Kodus näidata erinevaid sõnumeid korraga vormis lisamisel
